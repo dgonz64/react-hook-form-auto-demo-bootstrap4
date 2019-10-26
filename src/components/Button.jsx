ef40e9ed7797bae4353b9692a8f9313a729a1536
David González <braben@gmail.com>
2019-10-26 13:35:51 +0100

    Better button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,18 +1,22 @@
 import React from 'react'
+import classnames from 'classnames'
 
 import styles from './Button.sass'
 
 export const Button = ({
   children,
-  onClick
+  onClick,
+  typeClass
 }) => {
   const defaulted = onClick ? e => {
     e.preventDefault()
     onClick()
   } : null
 
+  const classes = classnames('btn', typeClass || 'btn-primary')
+
   return (
-    <button href="#" onClick={defaulted} className={styles.button}>
+    <button href="#" onClick={defaulted} className={classes}>
       {children}
     </button>
   )
